4b63f2716368d9bb7ae30a38380c8a361ad8eb71
Alex Kiselyov <akiselyov@ukr.net>
2024-10-27 10:09:09 +0400

    Added TESA: Redguard

--- constants.py
+++ constants.py
@@ -1,3 +1,3 @@
 DB_PATH = 'db/_main.db'
 TABLE_NAME = 'glossary'
-VALID_GAMES = ['eso', 'skyrim', 'oblivion', 'morrowind', 'legends', 'blades', 'castles']
+VALID_GAMES = ['eso', 'skyrim', 'oblivion', 'morrowind', 'legends', 'blades', 'castles', 'redguard']
