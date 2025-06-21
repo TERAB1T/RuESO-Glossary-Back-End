7b38c38ac341f8068fcc273483c3b93a29382695
Alex Kiselyov <akiselyov@ukr.net>
2025-06-22 00:17:19 +0400

    Add Battlespire

--- glossary/constants.py
+++ glossary/constants.py
@@ -5,4 +5,4 @@ DB_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), DB_PATH_REL)
 
 TABLE_NAME = 'glossary'
 COLUMNS = ['game', 'type', 'en', 'ru']
-VALID_GAMES = ['eso', 'skyrim', 'oblivion', 'morrowind', 'legends', 'blades', 'castles', 'redguard']
+VALID_GAMES = ['eso', 'skyrim', 'oblivion', 'morrowind', 'legends', 'blades', 'castles', 'redguard', 'battlespire']
